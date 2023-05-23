bb9abf9e523ba3468c4948a53ebd066aa9f87f4b
Karina Velasquez <kvelasquez@xpanxion.com>
2023-05-23 19:04:55 -0400

    Update IT HW

---
+++ reactithw2/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import{HelloWorld} from './HelloWorld'
+
+function App() {
+  return (
+    <div className="App">
+     <HelloWorld />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ reactithw2/src/HelloWorld.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './HelloWorld.css'
+
+export const HelloWorld = () => {
+  return (
+    <div className='hello'>Hello, World!</div>
+  )
+}
